699905b83f15a4b6c1ce116ad116ff2aeefdc6ec
emma3004 <91117846+emma3004@users.noreply.github.com>
2022-01-12 21:04:30 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,16 @@
+//program to find whether palindrome
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      String word = input.next();
+
+
+
+
+
+
+
+    }
+
+}
